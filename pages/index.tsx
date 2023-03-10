869f3d9a4362ad025b5b093df5b259d43abf4fad
warrenjkim <wjkim2311@g.ucla.edu>
2023-03-10 00:53:57 -0800

    added querying to index
    * queries from the clubs collection in firebase
    * TODO: dynamically update club cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import Intro from '../components/Intro'
 import MyClubs from '../components/MyClubs'
 import AllClubs from '../components/AllClubs'
@@ -5,7 +7,23 @@ import AllClubs from '../components/AllClubs'
 import { UserContext } from '@/lib/context'
 import { useContext } from 'react'
 
-export default function Home() {
+import { firestore } from '@/lib/firebase'
+
+export async function getServerSideProps(context) {
+
+  const q = firestore.collection("clubs")
+  .orderBy('name');
+  console.log(q);
+
+  const clubs = (await q.get()).docs;
+  return {
+    props: { clubs: JSON.parse(JSON.stringify(clubs)) }, // will be passed to the page component as props
+  };
+}
+
+
+export default function Home({ clubs }) {
+  console.log(clubs)
   // get context from _app
   const { name, email, uid } = useContext(UserContext);
 
@@ -17,3 +35,4 @@ export default function Home() {
     </div>
   )
 }
+

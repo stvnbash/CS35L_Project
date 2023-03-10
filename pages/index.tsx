706ac62c5e15171c06bf450b91c76ac720dcd02a
warrenjkim <wjkim2311@g.ucla.edu>
2023-03-09 20:01:40 -0800

    fixed context so that it now passing through the component tree
    * removed bloat in _app and index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
-import { Inter } from '@next/font/google'
 import Intro from '../components/Intro'
 import MyClubs from '../components/MyClubs'
 import AllClubs from '../components/AllClubs'
-import { useUserData } from '@/lib/hooks'
 
-const inter = Inter({ subsets: ['latin'] })
+import { UserContext } from '@/lib/context'
+import { useContext } from 'react'
 
 export default function Home() {
-  const userData = useUserData();
-  const user = userData;
-  
+  // get context from _app
+  const { name, email, uid } = useContext(UserContext);
+
   return (
     <div>
       <Intro />
-      {user.username && <MyClubs />}
+      {name && <MyClubs />}
       <AllClubs />
     </div>
   )

d3b48028402a83aa38a2317db07a2b74aacdc804
Kalon Lucas Kelley <klucas.kelley@gmail.com>
2023-03-11 11:56:35 -0800

    Rewrote Calendar.tsx to allow for event querying from the database.
    * Removed previous function FullCalendar
    * Replaced with a new class FullCalendar to handle calendar redering
      and event handling
    * Pulls from the database when any page with the class is mounted
    * Stores all event data from events collection into the components state events
    * Then renders the calendar with preconfigured options and props as options with
      the fetched events as events

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,30 +1,56 @@
 import Calendar from '@fullcalendar/react';
 import dayGrid from '@fullcalendar/daygrid';
 import timeGrid from '@fullcalendar/timegrid'
+import { firestore } from "@/lib/firebase";
+import React, {Component} from "react";
 
 
+class FullCalendar extends Component {
+    constructor(props: string) {
+        super(props);
+        this.state = {
+            events: []
+        };
+    }
 
-export default function FullCalendar(props) {
-    return <Calendar plugins={[dayGrid, timeGrid]} {...props}
-        headerToolbar={{
-            start: 'today prev next',
-            center: "title",
-            end: "dayGridDay dayGridMonth timeGridWeekShort"
-        }}
-        events={[
-            { // this object will be "parsed" into an Event Object
-              title: 'ACM Hack', // a property!
-              allDay: false,
-              start: '2023-03-10T13:30:00-08:00', // a property!
-              end: '2023-03-10T15:30:00-08:00'
-            }
-        ]}
-        views={{
-            timeGridWeekShort: {
-                type: 'timeGridWeek',
-                slotMinTime: '06:00:00',
-                slotMaxTime: '22:00:00'
-            }
-        }}
-    />;
+
+    componentDidMount() {
+        firestore.collection('events').get().then(snapshot => {
+            const events = snapshot.docs.map(doc => doc.data());
+            this.setState({ events });
+        });
+    }
+
+
+    render() {
+        const eventArray = this.state.events.map(event => ({
+            title: event.title,
+            start: new Date(event.start),
+            end: new Date(event.end),
+            allDay: event.allDay,
+            color: 'purple'
+        }));
+
+        return (
+            <Calendar
+                plugins={[ timeGrid, dayGrid ]}
+                {...this.props}
+                events={eventArray}
+                headerToolbar={{
+                    start: 'today prev,next',
+                    center: 'title',
+                    end: 'dayGridDay,dayGridMonth,timeGridWeekShort'
+                }}
+                views={{
+                    timeGridWeekShort: {
+                        type: 'timeGridWeek',
+                        slotMinTime: '06:00:00',
+                        slotMaxTime: '22:00:00'
+                    }
+                }}
+                nowIndicator={true}
+            />
+        );
+    }
 }
+export default FullCalendar;

59c0c87e7f599cb98b3a561d882d48a5e208eee5
warrenjkim <blankmkb@gmail.com>
2023-03-10 18:34:25 -0800

    bug with loading personal club cards. need help
    * personal club cards will only load once and will not reload
      on refresh
    * added a joinedClubs field in context

--- components/MyClubs.tsx
+++ components/MyClubs.tsx
@@ -1,13 +1,14 @@
 import ClubCard from "./ClubCard";
 
 export default function Component({ myClubs }: { myClubs: any}) {
+    console.log(myClubs)
     return (
         <div className="mt-4 p-4 bg-slate-100 rounded-2xl">
             <h2 className="text-2xl">My Clubs</h2>
             {/* some map function to iterate over myClubs for logged in user */}
             <div className="grid grid-cols-3 gap-4 w-full p-4">
                 {myClubs.length === 0 && <p>Immerse yourself in UCLA!  Clubs you join will appear here!</p>}
-                {myClubs.map((club: any) => <ClubCard key={club.name} name={club.name} description={club.description} /> )}
+                {myClubs.map((myClub: any) => <ClubCard key={myClub.name} name={myClub.name} description={myClub.description} /> )}
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import MyClubs from "../components/MyClubs";
 import AllClubs from "../components/AllClubs";
 
 import { UserContext } from "@/lib/context";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { firestore } from "@/lib/firebase";
 
@@ -13,56 +13,46 @@ export async function getServerSideProps(context) {
   // club collection to get all clubs (alphabetic order)
   const clubCollection = firestore.collection("clubs").orderBy("name");
 
+  // get metadata for clubs
+  const clubsMetaData = (await clubCollection.get()).docs;
+
+  // get doc ids
+  const clubDocIDs = clubsMetaData.map((doc) => doc.id);
+
   // mapping docs to array of objects
-  const clubs = (await clubCollection.get()).docs.map((doc) => doc.data());
+  const clubs = clubsMetaData.map((doc) => doc.data());
+  
+  let clubsDict = {};
+  for(let i = 0; i < clubs.length; i++)
+    clubsDict[clubDocIDs[i]] = clubs[i];
+
   return {
     props: {
       clubs: JSON.parse(JSON.stringify(clubs)),
+      clubsDict: JSON.parse(JSON.stringify(clubsDict))
     }, // will be passed to the page component as props
   };
 }
 
-export default function Home({ clubs }) {
+export default function Home({ clubs, clubsDict }) {
   // get context from _app
-  const { name, email, uid } = useContext(UserContext);
-
-  // // async function to get clubs user is in
-  // const getMyClubIDs = async () => {
-  //   // get current user
-  //   const myClubIDsDoc = firestore.collection("users").doc(`${email}`);
-
-  //   // myClubs is the array of club *** ID's ***
-  //   let myClubIDs;
-  //   try {
-  //     myClubIDs = (await myClubIDsDoc.get()).data().clubs;
-  //   }
-  //   catch {
-  //     console.log("data() is undefined!");
-  //   }
-
-  //   return myClubIDs;    
-  // };
-
-  // const getMyClubs = () => {
-  //   // get the clubs collection
-  //   const clubsCollection = firestore.collection("clubs");
-  //   let myClubs = [];
-
-  //   // for each element in myClubIDs, find its corresponding data entry
-  //   let ids = getMyClubIDs();
-
-  //   for(let i = 0; i < ids.length; i++) {
-  //     const clubDoc = clubsCollection.get().doc(ids[i]);
-  //     myClubs.push(clubDoc.data())
-  //   }
-
-  //   return myClubs;
-  // };
+  const { name, email, uid, joinedClubs } = useContext(UserContext);
+  const [myClubs, setMyClubs] = useState([]);
+  //console.log(joinedClubs)
+  useEffect(() => {
+    if (joinedClubs) {
+      for(let i = 0; i < joinedClubs.length; i++) {
+        console.log(joinedClubs[i])
+        console.log(clubsDict[joinedClubs[i]])
+        myClubs.push(clubsDict[joinedClubs[i]]);
+      }
+    }
+  }, [joinedClubs]);
 
   return (
     <div>
       <Intro />
-      {name && <MyClubs myClubs={clubs} />}
+      {email && <MyClubs myClubs={myClubs} />}
       <AllClubs clubs={clubs} />
     </div>
   );

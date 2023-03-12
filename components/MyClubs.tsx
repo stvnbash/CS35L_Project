e97c7805c0575680901fc71ba95039db2486d475
Steven Bash <56286042+stvnbash@users.noreply.github.com>
2023-03-11 21:22:54 -0800

    No longer using AllClubs and MyClubs, commented out code

--- components/MyClubs.tsx
+++ components/MyClubs.tsx
@@ -1,15 +1,15 @@
 import ClubCard from "./ClubCard";
 
 export default function Component({ myClubs }: { myClubs: any }) {
-    console.log(myClubs)
-    return (
-        <div className="mt-4 p-4 bg-slate-100 rounded-2xl">
-            <h2 className="text-2xl">My Clubs</h2>
-            {/* some map function to iterate over myClubs for logged in user */}
-            {myClubs.length === 0 ? <p>Immerse yourself in UCLA!  Clubs you join will appear here!</p>
-            : <div className="grid grid-cols-3 gap-4 w-full p-4">
-                {myClubs.map((myClub: any) => <ClubCard key={myClub.id} name={myClub.name} description={myClub.description} />)}
-            </div>}
-        </div>
-    )
+    // console.log(myClubs)
+    // return (
+    //     <div className="mt-4 p-4 bg-slate-100 rounded-2xl">
+    //         <h2 className="text-2xl">My Clubs</h2>
+    //         {/* some map function to iterate over myClubs for logged in user */}
+    //         {myClubs.length === 0 ? <p>Immerse yourself in UCLA!  Clubs you join will appear here!</p>
+    //         : <div className="grid grid-cols-3 gap-4 w-full p-4">
+    //             {myClubs.map((myClub: any) => <ClubCard key={myClub.id} name={myClub.name} description={myClub.description} />)}
+    //         </div>}
+    //     </div>
+    // )
 }

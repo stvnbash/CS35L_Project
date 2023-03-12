d2850cdc0eeda02f656a86d99b8c5e6f7c13a85c
gibbsAR12 <talhavabadin@gmail.com>
2023-03-12 01:40:19 -0800

    Check that a club does not exist when creating
    *Do not require some club fields when creating

--- pages/createClub.tsx
+++ pages/createClub.tsx
@@ -6,37 +6,49 @@ import { ChangeEvent, useState } from "react";
 import { db } from '@/lib/firebase'
 import { doc, setDoc } from 'firebase/firestore'
 import { query, where, getDoc, getDocs } from 'firebase/firestore'
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, updateDoc, limit } from 'firebase/firestore'
 
-function inputCheck(clubName, clubDescription, email){
+async function inputCheck(clubName, clubDescription, email){
     console.log(email)
-    if (clubName === ""  && clubDescription === "") {
-        console.log("le")
-    } else {
-        console.log("me");
-        createClub(db, {clubName}, {clubDescription})
+    if (clubName !== ""  && clubDescription !== "") {
+        let data;
+        const q = query(collection(db, "clubs"), where("name", "==", clubName), limit(1));
+        const clubData = await getDocs(q);
+        clubData.forEach((doc) => {
+            data = doc.data();
+        });
+        console.log(data)
+        if(data === undefined){
+            createClub(db, {clubName}, {clubDescription})
+        } else {
+            console.log("fe")
+        }
+        
     }
 }
 
-async function editCheck(clubName, clubDescription){
-    console.log("here")
+async function editCheck(clubName, newClubDescription, newClubName){
+
     const q = query(collection(db, "clubs"), where("name", "==", clubName));
-
+ 
     const clubData = await getDocs(q);
-    
+    let id = 3;
+    let data;
     clubData.forEach((doc) => {
-        console.log(doc.id);
-        console.log(doc.data())
+        id = doc.id;
+        data = doc.data();
     });
-
+ 
+    if(newClubDescription === ""){
+        newClubDescription = data.description
+    }
+    
+    await updateDoc(doc(db, "clubs", id), {
+      name: newClubName,
+      description: newClubDescription
+    });
 }
 
-async function editClub(db, {clubName}, {clubDescription}) {
-    await setDoc(doc(db, "clubs", "p"), {
-        name: clubName,
-        description: clubDescription
-      });
-}
 async function createClub(db, {clubName}, {clubDescription}) {
     await addDoc(collection(db, "clubs"), {
         name: clubName,
@@ -47,6 +59,8 @@ async function createClub(db, {clubName}, {clubDescription}) {
 export default function hate() {
   const [clubName, setClubName] = useState("");
   const [clubDescription, setClubDescription] = useState("")
+  const [newClubName, setNewClubName] = useState("");
+  const [newClubDescription, setNewClubDescription] = useState("")
   const { name, email, uid } = useContext(UserContext);
 
   const getClubName = (e: ChangeEvent<HTMLInputElement>) => {
@@ -57,6 +71,14 @@ export default function hate() {
     //Store the input value to local state
     setClubDescription(e.target.value);
   };
+  const getNewClubName = (e: ChangeEvent<HTMLInputElement>) => {
+    //Store the input value to local state
+    setNewClubName(e.target.value);
+  }
+  const getNewClubDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    //Store the input value to local state
+    setNewClubDescription(e.target.value);
+  };
 
   return (
     <div>
@@ -65,8 +87,15 @@ export default function hate() {
 
       <input type="text" onChange={getClubDescription} value={clubDescription} />
       <p>Input: {clubDescription}</p>
+
+      <input type="text" onChange={getNewClubName} value={newClubName} />
+      <p>Input: {newClubName}</p>
+
+      <input type="text" onChange={getNewClubDescription} value={newClubDescription} />
+      <p>Input: {newClubDescription}</p>
+
       <button onClick={() => inputCheck(clubName, clubDescription, email)}>Create Club</button>
-      <button onClick={() => editCheck(clubName, clubDescription)}>Edit Club</button>
+      <button onClick={() => editCheck(clubName, newClubName, newClubDescription)}>Edit Club</button>
     </div>
   );
 };

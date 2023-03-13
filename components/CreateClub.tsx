b0095fc9b5df80b4f0a27ade89f1f4320e1ab508
gibbsAR12 <talhavabadin@gmail.com>
2023-03-13 15:26:08 -0700

    Implemented create club with css and styling
    *First merged createClub branch and createandedit branch
    *deleted pages/createClub.tsx
    *Then combined css styling in createandedit with logic for creating club from createClub

--- components/CreateClub.tsx
+++ components/CreateClub.tsx
@@ -4,12 +4,37 @@ import { UserContext } from "@/lib/context"
 import { useContext, ChangeEvent, useState } from "react"
 
 import { db } from '@/lib/firebase'
-import { doc, setDoc, query, where, getDoc, getDocs, collection, addDoc } from 'firebase/firestore'
+import { doc, setDoc, query, where, getDoc, getDocs, collection, addDoc, limit } from 'firebase/firestore'
 
-async function createClub(db, { clubName }, { clubDescription }) {
+async function inputCheck(clubName, clubDescription, email, clubInstagram, clubWebsite){
+    console.log(email)
+    console.log(clubInstagram)
+    console.log(clubWebsite)
+    if (clubName !== ""  && clubDescription !== "") {
+        let data;
+        const q = query(collection(db, "clubs"), where("name", "==", clubName), limit(1));
+        const clubData = await getDocs(q);
+        clubData.forEach((doc) => {
+            data = doc.data();
+        });
+        console.log(data)
+        if(data === undefined){
+            console.log("ef")
+            createClub(db, {clubName}, {clubDescription}, {email}, {clubInstagram}, {clubWebsite})
+        } else {
+            console.log("fe")
+        }
+    }
+}
+
+async function createClub(db, {clubName}, {clubDescription}, {email}, {clubInstagram}, {clubWebsite}) {
+    //let clubId = clubName.replace(/\s+/g, "")
     await addDoc(collection(db, "clubs"), {
         name: clubName,
-        description: clubDescription
+        description: clubDescription,
+        moderators: [email],
+        instagram: clubInstagram,
+        website: clubWebsite
     });
 }
 
@@ -17,6 +42,8 @@ export default function CreateClub() {
     const { name, email, uid } = useContext(UserContext)
     const [clubName, setClubName] = useState("");
     const [clubDescription, setClubDescription] = useState("")
+    const [clubWebsite, setWebsite] = useState("");
+    const [clubInstagram, setInstagram] = useState("")
 
     const getClubName = (e: ChangeEvent<HTMLInputElement>) => {
         //Store the input value to local state
@@ -26,13 +53,14 @@ export default function CreateClub() {
         //Store the input value to local state
         setClubDescription(e.target.value);
     };
-
-    function inputCheck(clubName, clubDescription, email) {
-        console.log(email)
-        if (clubName !== "" && clubDescription !== "") {
-            createClub(db, { clubName }, { clubDescription })
-        }
-    }
+    const getClubWebsite = (e: ChangeEvent<HTMLInputElement>) => {
+        //Store the input value to local state
+        setWebsite(e.target.value);
+      };
+      const getClubInstagram = (e: ChangeEvent<HTMLInputElement>) => {
+        //Store the input value to local state
+        setInstagram(e.target.value);
+      };
 
     return (
         <div>
@@ -55,9 +83,23 @@ export default function CreateClub() {
                     placeholder="Club Description"
                     onChange={getClubDescription}
                     value={clubDescription} />
+                <h3 className="text-lg p-4">Enter Club Website</h3>
+                <input
+                    className="mb-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    type="url"
+                    placeholder="https://uclaclubhub.com"
+                    onChange={getClubWebsite}
+                    value={clubWebsite} />
+                <h3 className="text-lg p-4">Enter Club Instagram</h3>
+                <input
+                    className="mb-8 appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-1 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    type="url"
+                    placeholder="https://instagram.com/uclaclubhub"
+                    onChange={getClubInstagram}
+                    value={clubInstagram} />
                 <button
                     className="text-lg font-bold px-4 py-2 rounded-xl text-slate-100 bg-gradient-to-r from-emerald-500 to-sky-500"
-                    onClick={() => inputCheck(clubName, clubDescription, email)}>
+                    onClick={() => inputCheck(clubName, clubDescription, email, clubInstagram, clubWebsite)}>
                     Create Club
                 </button>
             </div>
